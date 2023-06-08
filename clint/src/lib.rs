7d83b992d834e6041951a969a08b0455ed87f555
Igor Spichkin <igorsa.ux@outlook.com>
2023-06-08 18:14:45 +0300

    update clint and rtc api

--- clint/src/lib.rs
+++ clint/src/lib.rs
@@ -6,8 +6,8 @@ pub const MMIO_ADDRESS: usize = 0x3000;
 
 const GET_TIMER_REGISTER: usize = 0x0;
 const GET_TIME_CMP_REGISTER: usize = 0x1;
-const REBASE_TIMER_REGISTER: usize = 0x0;
-const SET_TIME_CMP_REGISTER: usize = 0x1;
+const SET_TIME_CMP_REGISTER: usize = 0x0;
+const MSOFTWARE_INTERRUPT: usize = 0x1;
 
 pub struct Clint {
     mmio: Mmio,
@@ -22,14 +22,13 @@ impl Clint {
         self.mmio.read_usize(GET_TIME_CMP_REGISTER)
     }
 
-    #[deprecated = "Does not work"]
-    pub unsafe fn rebase_timer(&mut self, value: usize) {
-        unimplemented!()
-        // self.mmio.write_usize(value, REBASE_TIMER_REGISTER);
-    }
-
     pub unsafe fn set_time_cmp(&mut self, value: usize) {
         self.mmio.write_usize(value, SET_TIME_CMP_REGISTER);
+    }
+
+    pub unsafe fn set_msoftware_interrupt(&mut self, state: bool) {
+        self.mmio
+            .write_u8(if state { 1 } else { 0 }, MSOFTWARE_INTERRUPT);
     }
 }
 
--- rtc/src/lib.rs
+++ rtc/src/lib.rs
@@ -6,6 +6,9 @@ use mmio::Mmio;
 
 pub const MMIO_ADDRESS: usize = 0x1000;
 
+const TIME_REGISTER: usize = 0x0;
+const SCHEDULE_INTERRUPT_REGISTER: usize = 0x0;
+
 #[derive(Debug, Clone, Copy)]
 pub struct Rtc {
     pub mmio: Mmio,
@@ -13,7 +16,16 @@ pub struct Rtc {
 
 impl Rtc {
     pub unsafe fn now(&self) -> Duration {
-        Duration::from_millis(self.mmio.read_u64(0))
+        Duration::from_millis(self.mmio.read_u64(TIME_REGISTER))
+    }
+
+    pub unsafe fn schedule_interrupt(&mut self, target: Duration) {
+        self.mmio
+            .write_u64(target.as_millis() as u64, SCHEDULE_INTERRUPT_REGISTER);
+    }
+
+    pub unsafe fn clear_interrupt(&mut self) {
+        self.mmio.write_u64(0, SCHEDULE_INTERRUPT_REGISTER);
     }
 }
 
